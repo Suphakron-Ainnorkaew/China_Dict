917ee1e4185004a75623d3b7cc908d6543eb9440
suphakron <suphakroninnorkrew@gmail.com>
2025-07-26 19:36:59 +0700

    @k, checkadmin

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,6 +5,7 @@ const authMiddleware = (requiredRole) => async (req, res, next) => {
   if (!token) return res.status(401).json({ message: 'No token provided' });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('Decoded JWT:', decoded);
     req.user = decoded;
     if (requiredRole) {
       if (Array.isArray(requiredRole)) {
